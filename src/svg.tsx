92baea6482c8b47f5d1f3e5bd18a880fe6210437
Ron Young <admin@ryoung.io>
2021-01-19 17:12:30 -0600

    possible pattern for hatching

--- src/svg.tsx
+++ src/svg.tsx
@@ -5,6 +5,12 @@ import { h } from 'preact'
 export const Icon = ({ options, width, height, x, y, style = {} }: {options: IconOptions, width: number, height: number, x?: number, y?: number, style?: Style}) => {
   return (
     <svg x={x} y={y} width={width} height={height} preserveAspectRatio='none' viewBox='0 0 20 20' style={{overflow: 'visible', ...style}}>
+      {/* <pattern id="diagonalHatch" patternUnits="userSpaceOnUse" width="4" height="4">
+        <path d="M-1,1 l2,-2
+                M0,4 l4,-4
+                M3,5 l2,-2"
+              style="stroke:black; stroke-width:1" />
+      </pattern> */}
       <Shape options={options} />
     </svg>
   )
